daf5b2c6bfd3e41947d3aad519f2ac2014689fb9
mymislife <mymislife@gmail.com>
2020-04-27 19:22:16 +0100

    My First editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
+import {schema} from "prosemirror-schema-basic";
+import {EditorState} from "prosemirror-state";
+import {EditorView} from "prosemirror-view";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const editor = useRef(null);
+    useEffect(() => {
+        const state = EditorState.create({schema});
+        const view = new EditorView(editor.current, {state});
+    });
+
+    return (
+        <div className="App">
+            <div ref={editor}/>
+        </div>
+    );
 }
 
 export default App;
