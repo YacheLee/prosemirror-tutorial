9fafd96d0a5b53d475da18a2745698fc031c8c05
mymislife <mymislife@gmail.com>
2020-04-27 22:30:40 +0100

    bold with hotkey

--- src/components/RichTextEditor/plugins/BoldPlugin.js
+++ src/components/RichTextEditor/plugins/BoldPlugin.js
@@ -3,22 +3,24 @@ import ReactDOM from "react-dom";
 import {MdFormatBold} from 'react-icons/md';
 import {Plugin} from 'prosemirror-state';
 import {toggleMark} from "prosemirror-commands";
+import {keydownHandler} from "prosemirror-keymap";
 
-function isActive(editorView, type) {
-    return !!markActive(editorView.state, type);
+function isActive(editorState, type) {
+    return !!markActive(editorState, type);
 }
 
 function markActive(state, type) {
-    const ref = state.selection;
-    const from = ref.from;
-    const $from = ref.$from;
-    const to = ref.to;
-    const empty = ref.empty;
+    const {selection} = state;
+    const {from, $from, to, empty} = selection;
     if (empty) {
         return type.isInSet(state.storedMarks || $from.marks());
     } else {
         return state.doc.rangeHasMark(from, to, type);
     }
+}
+
+function toggleBold(editorState, dispatch){
+    toggleMark(editorState.schema.marks.strong, {strong: true})(editorState, dispatch);
 }
 
 class BoldView{
@@ -30,12 +32,12 @@ class BoldView{
     renderReactComponent(editorView){
         const markType = editorView.state.schema.marks.strong;
 
-        const isUsing = isActive(editorView, markType);
+        const isBolding = isActive(editorView.state, markType);
 
-        ReactDOM.render(<MdFormatBold style={{color: isUsing ? "blue" : "black"}} onClick={e=>{
+        ReactDOM.render(<MdFormatBold style={{color: isBolding ? "blue" : "black"}} onClick={e=>{
             e.preventDefault();
             editorView.focus();
-            toggleMark(markType, {strong: true})(editorView.state, editorView.dispatch);
+            toggleBold(editorView.state, editorView.dispatch);
         }} />, this.dom);
     }
     update(editorView){
@@ -49,6 +51,13 @@ const BoldPlugin = new Plugin({
         const view = new BoldView(editorView);
         editorView.dom.parentNode.insertBefore(view.dom, editorView.dom);
         return view;
+    },
+    props: {
+        handleKeyDown: keydownHandler({
+            "Mod-b": (editorState, dispatch)=>{
+                toggleBold(editorState, dispatch);
+            }
+        })
     },
     update(){
         return true;
