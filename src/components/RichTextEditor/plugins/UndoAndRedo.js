f34f79316933efc3abf0b683f977936bf74c9d6d
mymislife <mymislife@gmail.com>
2020-04-27 19:39:44 +0100

    more basic plugin for redo and undo

--- src/components/RichTextEditor/plugins/UndoAndRedo.js
+++ src/components/RichTextEditor/plugins/UndoAndRedo.js
@@ -1,4 +1,12 @@
 import {redo, undo} from "prosemirror-history";
-import {keymap} from "prosemirror-keymap";
+import {keydownHandler} from "prosemirror-keymap";
+import {Plugin} from 'prosemirror-state';
 
-export default keymap({"Mod-z": undo, "Mod-y": redo});
+export default new Plugin({
+    props: {
+        handleKeyDown: keydownHandler({
+            "Mod-z": undo,
+            "Mod-y": redo
+        })
+    }
+});
