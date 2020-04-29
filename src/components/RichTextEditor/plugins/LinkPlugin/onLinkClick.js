3003d6758dfe38bc2b532f6f4e9af6a588c12580
ya-che.li <ya-che.li@and.digital>
2020-04-29 23:46:33 +0100

    copy the link

--- src/components/RichTextEditor/plugins/LinkPlugin/onLinkClick.js
+++ src/components/RichTextEditor/plugins/LinkPlugin/onLinkClick.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import copy from 'copy-to-clipboard';
 import {setPopoverAnchorElement, setPopoverContent} from '../../RichTextEditor';
 import LinkPopover from './components/LinkPopover';
 import setLinkHref from './setLinkHref';
@@ -7,6 +8,10 @@ function onLinkClick(editorView, hyperlinkElement, mark){
     const url = hyperlinkElement.getAttribute('href');
     setPopoverContent(<LinkPopover
         url={url}
+        onCopyLink={()=>{
+            copy(url);
+            setPopoverAnchorElement(null);
+        }}
         onRemoveLink={()=>{
             setLinkHref('', mark.pos)(
                 editorView.state,
