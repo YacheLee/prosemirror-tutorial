ef7ad272dee9e57ee8602c47615ca7b771870d8c
ya-che.li <ya-che.li@and.digital>
2020-04-30 00:38:42 +0100

    the edit link UI

--- src/components/RichTextEditor/plugins/LinkPlugin/onLinkClick.js
+++ src/components/RichTextEditor/plugins/LinkPlugin/onLinkClick.js
@@ -3,11 +3,17 @@ import copy from 'copy-to-clipboard';
 import {setPopoverAnchorElement, setPopoverContent} from '../../RichTextEditor';
 import LinkPopover from './components/LinkPopover';
 import setLinkHref from './setLinkHref';
+import LinkEditPopover from './components/LinkEditPopover';
 
 function onLinkClick(editorView, hyperlinkElement, mark){
     const url = hyperlinkElement.getAttribute('href');
+    const text = hyperlinkElement.innerText;
+
     setPopoverContent(<LinkPopover
         url={url}
+        onEditLink={()=>{
+            setPopoverContent(<LinkEditPopover url={url} text={text} />);
+        }}
         onCopyLink={()=>{
             copy(url);
             setPopoverAnchorElement(null);
