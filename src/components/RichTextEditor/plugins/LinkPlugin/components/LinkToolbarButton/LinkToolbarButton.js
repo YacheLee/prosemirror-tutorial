f8457ab46ea23a272174a9c5814335e3cb5110d7
ya-che.li <ya-che.li@and.digital>
2020-05-01 21:55:32 +0100

    toggle the link

--- src/components/RichTextEditor/plugins/LinkPlugin/components/LinkToolbarButton/LinkToolbarButton.js
+++ src/components/RichTextEditor/plugins/LinkPlugin/components/LinkToolbarButton/LinkToolbarButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {MdInsertLink} from 'react-icons/md';
 import ToolbarButtonStyle from '../../../shared/ToolbarButtonStyle';
 import LinkEditPopover from '../LinkEditPopover';
@@ -6,14 +6,24 @@ import insertLink from './insertLink';
 import {setPopoverAnchorElement, setPopoverContent} from '../../../../RichTextEditor';
 
 function LinkToolbarButton({editorView}){
+    const [open, setOpen] = useState(false);
+
     return <ToolbarButtonStyle onClick={event => {
-        setPopoverAnchorElement(event.target);
-        setPopoverContent(
-            <LinkEditPopover onApply={({text, url}) => {
-                insertLink(editorView.state.selection.from, url, text)(editorView.state, editorView.dispatch);
-                setPopoverAnchorElement(null);
-            }}/>);
-        }}>
+        if(!open){
+            setOpen(true);
+            setPopoverAnchorElement(event.target);
+            setPopoverContent(
+                <LinkEditPopover onApply={({text, url}) => {
+                    insertLink(editorView.state.selection.from, url, text)(editorView.state, editorView.dispatch);
+                    setPopoverAnchorElement(null);
+                    setOpen(false);
+                }}/>);
+        }
+        else{
+            setPopoverAnchorElement(null);
+            setOpen(false);
+        }
+    }}>
         <MdInsertLink/>
     </ToolbarButtonStyle>
 }
