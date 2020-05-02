5041fbdcecd182601a9ae77494a919b4c791f65f
ya-che.li <ya-che.li@and.digital>
2020-05-02 15:10:30 +0100

    highlight to attach the link

--- src/components/RichTextEditor/plugins/LinkPlugin/components/LinkToolbarButton/LinkToolbarButton.js
+++ src/components/RichTextEditor/plugins/LinkPlugin/components/LinkToolbarButton/LinkToolbarButton.js
@@ -4,6 +4,14 @@ import ToolbarButtonStyle from '../../../shared/ToolbarButtonStyle';
 import LinkEditPopover from '../LinkEditPopover';
 import insertLink from './insertLink';
 import PopoverManager from '../../../../PopoverManager';
+import setLinkHref from '../../setLinkHref';
+
+function getSelectedText(state, from, to) {
+    const selectedNode = state.doc.cut(from, to);
+    const selectedFragment = selectedNode.content;
+
+    return selectedFragment.content.map(e=>e.textContent).join("");
+}
 
 function LinkToolbarButton({editorView, toolbarButtonDom}){
     return <ToolbarButtonStyle onClick={event => {
@@ -14,10 +22,24 @@ function LinkToolbarButton({editorView, toolbarButtonDom}){
             PopoverManager.closePopover();
         }
         else{
+            let text = "";
+            const {from, to} = editorView.state.selection;
+            const isInsertMode = from === to;
+            if(!isInsertMode){
+                text = getSelectedText(editorView.state, from, to);
+            }
+
             PopoverManager.setPopoverAnchorElement(toolbarButtonDom);
             PopoverManager.setPopoverContent(
-                <LinkEditPopover onApply={({text, url}) => {
-                    insertLink(editorView.state.selection.from, url, text)(editorView.state, editorView.dispatch);
+                <LinkEditPopover
+                    text={text}
+                    onApply={({text, url}) => {
+                    if(isInsertMode){
+                        insertLink(from, url, text)(editorView.state, editorView.dispatch);
+                    }
+                    else{
+                        setLinkHref(url, from ,to)(editorView.state, editorView.dispatch);
+                    }
                     PopoverManager.closePopover();
                 }}/>
             );
